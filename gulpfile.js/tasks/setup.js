4521d250c87118977abd3f323115ed97c1b74a57
Dean Morgan <deanmorgan@mediablox.co.uk>
2019-09-09 22:13:06 +0100

    Updated setup.js task

--- gulpfile.js/tasks/setup.js
+++ gulpfile.js/tasks/setup.js
@@ -1,11 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
-var rename = require('gulp-rename');
 
 gulp.task('copy-main-scss', function(done) {
-	gulp.src('./node_modules/mb.mbfw/setup.main.scss')
-		.pipe(rename('main.scss'))
+	gulp.src('./node_modules/mb.mbfw.scss/main.scss')
 		.pipe(gulp.dest('./src/assets/scss'));
 	done();
 });
