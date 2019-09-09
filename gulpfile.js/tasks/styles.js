87adc84ab5e6593e647030460296f0e6bde87821
DeanMorganMB <deanmorgan@mediablox.co.uk>
2019-09-09 16:31:54 +0100

    Updates styles.js gulp task

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -1,12 +1,22 @@
 'use strict';
 
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var sass = require('gulp-sass');
 
-gulp.task('styles', function(done) {
-	console.log("styles:start");
 
-	console.log("styles:done");
+gulp.task('scss', function(done) {
+	gulp.src('./src/assets/scss/main.scss')
+		.pipe(sourcemaps.init())
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([autoprefixer()])) // uses ".browserslistrc"
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist/assets/css'));
 	done();
 });
 
-gulp.task('styles', gulp.series('styles'));
+gulp.task('styles', gulp.series('scss'));
