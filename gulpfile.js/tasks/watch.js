f89be1cb12c3d0fe8d6025acd4a9b60bd734ffac
Dean Morgan <deanmorgan@mediablox.co.uk>
2019-09-10 22:20:42 +0100

    Added watch.js task

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,6 +1,37 @@
 'use strict';
 
 var gulp = require('gulp');
+var browser = require('browser-sync');
 
-// Builds the documentation and framework files
-gulp.task('watch', gulp.series('scss','javascript'));
+gulp.task('serve', function(done) {
+	browser.init({
+		server: 'dist',
+		port: 3000,
+		notify: {
+			styles: [
+				'pointer-events: none',
+				'position: fixed',
+				'bottom: 10px',
+				'left: 10px',
+				'text-align: center',
+				'background-color: rgba(24, 24, 48, 0.8)',
+				'color: #fff',
+				'padding: 10px',
+				'border: 2px solid #fff',
+				'font: 13px Menlo, Monaco, monospace',
+				'border-radius: 5px',
+			],
+		},
+	});
+	done();
+});
+
+gulp.task('reload', function(done) {
+  browser.reload();
+  done();
+});
+
+gulp.task('watch', function(done) {
+  gulp.watch('src/**/*.*').on('all', gulp.series('build', 'reload'));
+  done();
+});
