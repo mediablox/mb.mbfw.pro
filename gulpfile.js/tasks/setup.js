c4bfd35c29d5b76abe6f65073946746a7cd47c84
Dean Morgan <deanmorgan@mediablox.co.uk>
2019-09-28 16:07:20 +0100

    Updated setup.js task

--- gulpfile.js/tasks/setup.js
+++ gulpfile.js/tasks/setup.js
@@ -5,11 +5,7 @@ var gulp = require('gulp');
 gulp.task('copy-mbfw-scss', function(done) {
 	gulp.src('./node_modules/mb.mbfw.scss/main.scss')
 		.pipe(gulp.dest('./src/assets/scss'));
-	gulp.src('./node_modules/mb.mbfw.scss/settings/_settings.global.scss')
-		.pipe(gulp.dest('./src/assets/scss/settings'));
 	gulp.src('./node_modules/mb.mbfw.scss/settings/_settings.config.scss')
-		.pipe(gulp.dest('./src/assets/scss/settings'));
-	gulp.src('./node_modules/mb.mbfw.scss/settings/_settings.color.scss')
 		.pipe(gulp.dest('./src/assets/scss/settings'));
 	done();
 });
